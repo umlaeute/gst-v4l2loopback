c01e6b6707dc16119d7bfcce4b6101acc1a85203
iem@barron.iemnet <iem@barron.iemnet>
2010-08-26 13:31:43 +0200

    removed debian/
    LATER i hope to get this into debian proper.

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the v4lsink package
-#
-0 4	* * *	root	[ -x /usr/bin/v4lsink_maintenance ] && /usr/bin/v4lsink_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(v4lsink):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="v4lsink" command="/usr/bin/v4lsink"
