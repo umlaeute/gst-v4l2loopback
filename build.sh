b66e5820b5a69624220397911b9c953f445e566c
IOhannes m zmoelnig <zmoelnig@iem.at>
2010-08-26 16:36:32 +0200

    removed vala and cleaned up build-system

--- build.sh
+++
@@ -1,1 +0,0 @@
-valac --output=gst-app --vapidir=. --pkg posix --pkg v4l2 --pkg gstreamer-0.10 --pkg gtk+-2.0 --pkg gdk-x11-2.0 --pkg gstreamer-interfaces-0.10 --pkg gstreamer-video-0.10 gst-app.vala gst-v4lsink-loopback.vala
